891d5053021e33ac407677d90042899314cef205
Loek Spoelder <loek.spoelder@coderclass.nl>
2022-05-25 15:40:27 +0200

    More javascript but a little bit better.

--- javascript/terminal.js
+++ javascript/terminal.js
@@ -1,20 +1,22 @@
 // Input function for terminal.
-var terminput = document.getElementById("terminput");
-var termoutput = document.getElementById("termoutput");
+var input = document.getElementById("terminput");
+var output = document.getElementById("termoutput");
 
-function terminal(ele) {
-    if(event.key === 'Enter') {
-        alert(ele.value);
-        if (terminput = "hallo") {
-          termoutput.innerHTML = "Het getal is hoger.";
-        }
-        else if (terminput = something) {
-          feedback.innerHTML = "Het getal is lager.";
-        }
-        else {
-          feedback.innerHTML = "Je hebt het getal geraden! Je hebt " + teller + " keer moeten raden.";
-        }
-    }
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("termbutton").click();
+  }
+});
+
+function terminal() {
+  if (input = "hallo") {
+    output.innerHTML = "hoi";
+  }
+  else if (input = something) {
+    feedback.innerHTML = "Het getal is lager.";
+  }
+  else {
+    feedback.innerHTML = "Je hebt het getal geraden! Je hebt " + teller + " keer moeten raden.";
+  }
 }
-
-terminal()
