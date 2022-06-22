93f724d1ab68b10aa0ccf250564726f653c1ed61
Loek Spoelder <loek.spoelder@coderclass.nl>
2022-06-22 14:15:36 +0200

    Site's done

--- javascript/terminal.js
+++ javascript/terminal.js
@@ -10,13 +10,17 @@ input.addEventListener("keypress", function(event) {
 });
 
 function terminal() {
-  if (input = "hoi") {
-    output.innerHTML = "Kapot";
+  if (input.value === "help") {
+    output.innerHTML = "Current available commands:<br>&nbsp;<br>'help' - List of commands.<br>'about' - About the maker.<br>'contact' - How to contact the maker.";
   }
-  else if (input = "werk") {
-    output.innerHTML = "HET WERKT";
+  else if (input.value === "about") {
+    output.innerHTML = "My name is Loek, I make stuff.<br>&nbsp;<br>In all seriousness, I am a multilingual programmer who's currently busy learning the programming so I can actually become the programmer I said I was in the begining of the sentence. Here's a picture of me:<br>&nbsp;<br>((((((((((((/////////////*****<br>((((((((((((((((//////////////<br>#####(#(####(#####(#((((((((((<br>%%%%%#%&%#%/###(/**,*/#(######<br>&&&&%%##*/(%#(/,,,....,/%#####<br>#####%/*/#%%%#(/*,......,((,,,<br>/////(**#%#(((/******,...,,,,,<br>///,,,*,/,/,,/%(,.&nbsp;..,&nbsp;&nbsp;******<br>##(####&@&&&&&@#///**//,,,,,,.<br>**///*/&&&%#&#/***(((//.,,,,,*<br>*////..%#(#&&(,,,,/*,******,,.<br>%%%%%&&%((/###,.,,,.,,,,,,,,,,<br>,,*****&//(#/*,,,,**,********,<br>///////, #*///**,,,,* //////**<br>%#%@((,..##(*,,..,,,.&nbsp;&nbsp;.**,&nbsp;&nbsp;&nbsp;<br>,,.,,,..&nbsp;.&&%(/***,.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;...";
+  }
+  else if (input.value === "contact") {
+    output.innerHTML = "You can contact me at loek.spoelder@coderclass.nl";
   }
   else {
-    output.innerHTML = "ja typ iets in man";
+    output.innerHTML = "'" + input.value + "' is not recognized as an internal or external command,<br> operable program or batch file.<br>Try 'help' to get started." ;
   }
+  input.value = ""
 }
